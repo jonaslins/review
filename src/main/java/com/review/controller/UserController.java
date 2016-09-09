d44c12305ed1beabcd7f2d849a3611eee1712f4f
Jonas Lins <jal3@cin.ufpe.br>
2016-09-08 21:15:20 -0300

    form changes

--- src/main/java/com/review/controller/UserController.java
+++ src/main/java/com/review/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.review.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.review.model.User;
 import com.review.service.UserService;
@@ -19,13 +24,39 @@ public class UserController {
 
 	@RequestMapping("/{id}") // localhost:8080/{appName}/user/1
 	public String show(@PathVariable Long id, Map<String, Object> model) {
-		
+
 		User user = userService.getUserById(id);
-		
+
 		model.put("user", user);
-		
+
 		//View that will be rendered after the setup we made above
 		return "user/show";
+
+	}
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	@ResponseBody
+	public List<User> getTags(@RequestParam String name) {
+
+		return simulateSearchResult(name);
+
+	}
+
+	private List<User> simulateSearchResult(String name) {
+
+		List<User> result = new ArrayList<User>();
 		
+		List<User> data = new ArrayList<User>();
+		data.add(new User("Jonas", "foo@bar"));
+		data.add(new User("José", "fofo@bar"));
+
+		// iterate a list and filter by tagName
+		for (User user : data) {
+			if (user.getName().toLowerCase().contains(name.toLowerCase())) {
+				result.add(user);
+			}
+		}
+
+		return result;
 	}
 }
