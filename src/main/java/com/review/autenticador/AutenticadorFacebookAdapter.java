4300ef8fae3de56888c2a3f80678f30ddadec0d5
Jonas Lins <jal3@cin.ufpe.br>
2016-09-27 21:48:18 -0300

    facebook autentication adapter

--- src/main/java/com/review/autenticador/AutenticadorFacebookAdapter.java
+++ src/main/java/com/review/autenticador/AutenticadorFacebookAdapter.java
@@ -8,19 +8,16 @@ import org.springframework.web.context.request.WebRequest;
 import com.review.model.entidade.Usuario;
 
 @Component("autenticadorAdapter")
-public class AutenticadorAdapter implements IAutenticador {
+public class AutenticadorFacebookAdapter implements IAutenticador {
 
 	@Autowired
 	private FacebookAutenticator facebookAutenticator;
 
 	@Override
 	public Usuario autenticar(WebRequest request, String redeSocial) {
-		if (redeSocial.equalsIgnoreCase("facebook")) {
-			User user = facebookAutenticator.signInWithFacebook(request);
-			Usuario usuario = new Usuario(user.getName(), user.getEmail());
-			return usuario;
-		}
-		return null;
+		User user = facebookAutenticator.signInWithFacebook(request);
+		Usuario usuario = new Usuario(user.getName(), user.getEmail());
+		return usuario;
 	}
 
 }
